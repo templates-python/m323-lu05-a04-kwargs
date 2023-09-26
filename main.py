411cfba0cdf9ad335534b3d39bf54b1adf556eec
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-26 11:39:32 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-def calculate():
+def user_profile(**kwargs):
     """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    Creates a formatted user profile string based on the provided keyword arguments.
 
+    Parameters:
+       **kwargs: Variable number of keyword arguments representing user data.
+
+    Returns:
+        str: A formatted string containing all the provided user data.
+    """
+    ...
 
 if __name__ == '__main__':
-    calculate()
+    # Teste deine Funktion
+    print(user_profile(name="Alice", age=30))  # Erwarteter Output: "Name: Alice, Age: 30"
--- main_test.py
+++ main_test.py
@@ -1,6 +1,10 @@
-import main
+from main import user_profile
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_user_profile_no_args():
+    assert user_profile() == "", "Should return an empty string when no keyword arguments are passed"
+
+def test_user_profile_single_arg():
+    assert user_profile(name="Alice") == "Name: Alice", "Should return a formatted string for a single keyword argument"
+
+def test_user_profile_multiple_args():
+    assert user_profile(name="Alice", age=30) == "Name: Alice, Age: 30", "Should return a formatted string for multiple keyword arguments"
